540ef7b3028162f3513fe3e84a45a24f7932c4da
Wesley A. Bickford <bickford@umich.edu>
2017-03-01 10:03:40 -0500

    created an nmds plot

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -46,3 +46,6 @@ code/mothur/mothur code/get_shared_otus.batch
 
 # generate the nmds files
 code/mothur/mothur code/get_nmds_data.batch
+
+# generate nmds figure using R script
+R -e "source('code/plot_nmds.R'); plot_nmds('data/mothur/stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.thetayc.0.03.lt.ave.nmds.axes')"
