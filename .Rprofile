af1b85e92f75117c05e133d00b469f6c1cac3231
Wesley A. Bickford <bickford@umich.edu>
2017-03-01 13:59:45 -0500

    attempt to reproduce the scaling up section of the paper with in line r code

--- .Rprofile
+++ .Rprofile
@@ -8,7 +8,7 @@ library("rmarkdown")
 ## output directory for figures
 if (require("knitr")) {
     opts_chunk$set(fig.path="results/figures/")
-    opts_knit$set(base.dir=normalizePath(getwd()))
+#    opts_knit$set(base.dir=normalizePath(getwd()))
     opts_knit$set(root.dir=normalizePath(getwd()))
 }
 
--- analysis_driver.bash
+++ analysis_driver.bash
@@ -49,3 +49,6 @@ code/mothur/mothur code/get_nmds_data.batch
 
 # generate nmds figure using R script
 R -e "source('code/plot_nmds.R'); plot_nmds('data/mothur/stability.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.thetayc.0.03.lt.ave.nmds.axes')"
+
+# Render R markdown file
+R -e "render('submission/practice.Rmd')"
